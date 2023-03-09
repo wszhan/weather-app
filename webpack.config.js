5dfb4b8d409ad7e44a880e4ee0d373442487102c
wszhan <wszhan@@users.noreply.github.com>
2023-03-09 17:59:56 -0500

    set up project files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/javascript/index.js',
+        api: './src/javascript/weatherAPI.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            template: './src/template.html',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    }
+};
